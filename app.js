6c7ba4dfd7953fcabea60aa47b4fd21063afd753
shrabon-sarker <bikash.webdeveloper5@gmail.com>
2021-01-16 15:29:29 +0600

    object add array function add

--- app.js
+++ app.js
@@ -4,3 +4,15 @@ console.log(n)
 
 const myFun = (a, b) => a*b;
 console.log(myFun(2,8))
+
+const obj = {
+    name : "this is a name biaksh",
+    age: 12,
+    print(){
+        setTimeout(() => {
+            console.log(name)
+        }, 1000);
+    }
+}
+
+obj.print();
